c6ba36e5cf5333951a0a24f85a36809a38c76f4d
Ohad <ohad@salaryo.com>
2020-12-25 14:44:27 +0200

    Client is without typescript, adds map

---
+++ client/src/components/Map.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true"
+              data-projection="EPSG:4326" style="height: 400px">
+        <vl-view v-model:zoom="zoom" v-model:center="center" v-model:rotation="rotation"></vl-view>
+
+        <vl-geoloc @update:position="geolocPosition = $event">
+          <template>
+            <vl-feature v-if="coordinates && coordinates.length">
+              <vl-geom-multi-point :coordinates="coordinates"></vl-geom-multi-point>
+            </vl-feature>
+          </template>
+        </vl-geoloc>
+
+        <vl-layer-tile id="osm">
+          <vl-source-osm></vl-source-osm>
+        </vl-layer-tile>
+      </vl-map>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Map",
+  props: {
+    coordinates: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      zoom: 2,
+      center: [0, 0],
+      rotation: 0,
+      geolocPosition: undefined,
+    }
+  },
+}
+</script>
---
+++ client/src/pages/HomePage/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Parking Spots Map</h1>
+    <Map :coordinates="[]" />
+  </div>
+</template>
+
+<script>
+import Map from "@/components/Map";
+
+export default {
+  name: '',
+  components: {
+    Map
+  }
+}
+</script>
